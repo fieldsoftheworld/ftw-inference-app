550cb770d7700e7f9a3f498c6f1a4a4adcd5d6c0
Zachery Thomas Richardet <zachery@Mac.attlocal.net>
2025-04-15 15:50:09 -0500

    getting map onto the screen

---
+++ map/src/components/Map.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { Map, View } from 'ol'
+import TileLayer from 'ol/layer/Tile'
+import OSM from 'ol/source/OSM'
+
+const map = ref<Map | null>(null)
+
+onMounted(() => {
+  map.value = new Map({
+    target: 'map',
+    layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+    ],
+    view: new View({
+      center: [0, 0],
+      zoom: 2,
+    }),
+  })
+})
+</script>
+
+<template>
+  <div id="map" class="map-container" />
+</template>
+
+<style scoped>
+#map {
+  width: 100%;
+  height: 100%;
+}
+</style>
