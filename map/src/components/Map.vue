86a62fb849ee29cee86540746241e1b7cf038231
Zachery Thomas Richardet <zachery@Mac.attlocal.net>
2025-05-07 15:48:33 -0500

    add some styling and the s2 layer to the map

--- map/src/components/Map.vue
+++ map/src/components/Map.vue
@@ -2,7 +2,7 @@
 import { ref, onMounted } from 'vue'
 import { Map, View } from 'ol'
 import TileLayer from 'ol/layer/Tile'
-import OSM from 'ol/source/OSM'
+import XYZ from 'ol/source/XYZ'
 
 const map = ref<Map | null>(null)
 
@@ -11,7 +11,12 @@ onMounted(() => {
     target: 'map',
     layers: [
       new TileLayer({
-        source: new OSM(),
+        source: new XYZ({
+          url: 'https://tiles.maps.eox.at/wmts?layer=s2cloudless-2024_3857&style=default&tilematrixset=GoogleMapsCompatible&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix={z}&TileCol={x}&TileRow={y}',
+          maxZoom: 19,
+          tileSize: 256,
+          attributions: 'Sentinel-2 cloudless imagery by <a href="https://eox.at">EOX</a>'
+        }),
       }),
     ],
     view: new View({
@@ -26,4 +31,25 @@ onMounted(() => {
   <div id="map" class="map-container"></div>
 </template>
 
-<style scoped></style>
+<style scoped>
+:deep(.ol-control button){
+  background-color: rgba(0, 0, 0, 0.6);
+  color: #FFF;
+}
+:deep(.ol-control button:hover){
+  background-color: rgba(0, 0, 0, 0.8);
+  color: #FFF;
+}
+:deep(.ol-attribution){
+  background-color: rgba(0, 0, 0, 0.6);
+  color: #FFF;
+}
+:deep(.ol-attribution ul){
+  color: #FFF;
+  font-size: 0.875rem;
+  text-shadow: none
+}
+:deep(.ol-attribution ul li a){
+  color: #FFF;
+}
+</style>
