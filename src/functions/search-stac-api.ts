e14ba6aeaf8fe2a6545b595f461b91abda566859
Zachery Thomas Richardet <zachery@Mac.lan>
2025-07-28 11:46:34 -0500

    add in area coverage and cloud cover filter

--- src/functions/search-stac-api.ts
+++ src/functions/search-stac-api.ts
@@ -67,28 +67,30 @@ export default async function searchStacApi(
   const startDate = (document.getElementById('start-date') as HTMLInputElement)?.value
   const endDate = (document.getElementById('end-date') as HTMLInputElement)?.value
   const cloudCover = (document.getElementById('cloud-cover') as HTMLInputElement)?.value || 10
+  const areaCoverage = (document.getElementById('area-coverage') as HTMLInputElement)?.value || 40
 
   try {
-    // Build query parameters
-    const params = new URLSearchParams()
-
-    // Add collection parameter
-    params.append('collections', 'sentinel-2-l2a')
-
-    // Add limit parameter
-    params.append('limit', '20')
-
-    // Add cloud cover filter using CQL2 text
-    params.append('filter-lang', 'cql2-text')
-    params.append('filter', `eo:cloud_cover<=${cloudCover}`)
+    // Build request body for POST
+    const requestBody: any = {
+      collections: ['sentinel-2-l2a'],
+      limit: 20,
+      query: {
+        ['eo:cloud_cover']: {
+          lte: cloudCover,
+        },
+        ['s2:nodata_pixel_percentage']: {
+          lte: areaCoverage,
+        },
+      },
+    }
 
     // Add datetime filter if dates are provided
     if (startDate && endDate) {
-      params.append('datetime', `${startDate}/${endDate}`)
+      requestBody.datetime = `${startDate}/${endDate}`
     } else if (startDate) {
-      params.append('datetime', `${startDate}/..`)
+      requestBody.datetime = `${startDate}/..`
     } else if (endDate) {
-      params.append('datetime', `../${endDate}`)
+      requestBody.datetime = `../${endDate}`
     }
 
     // Add bbox parameter if provided
@@ -103,23 +105,22 @@ export default async function searchStacApi(
       const [minLon, minLat] = transform([minX, minY], 'EPSG:3857', 'EPSG:4326')
       const [maxLon, maxLat] = transform([maxX, maxY], 'EPSG:3857', 'EPSG:4326')
 
-      params.append('bbox', `${minLon},${minLat},${maxLon},${maxLat}`)
+      requestBody.bbox = [minLon, minLat, maxLon, maxLat]
     }
 
     // Add the pagination token if we're loading more results
     if (!resetSearch && nextPageToken) {
-      params.append('next', nextPageToken)
+      requestBody.next = nextPageToken
     }
 
-    // Construct the URL with query parameters
-    const searchUrl = `https://earth-search.aws.element84.com/v1/search?${params.toString()}`
-
-    // Make the GET request
-    const response = await fetch(searchUrl, {
-      method: 'GET',
+    // Make the POST request
+    const response = await fetch('https://earth-search.aws.element84.com/v1/search', {
+      method: 'POST',
       headers: {
+        'Content-Type': 'application/json',
         Accept: 'application/geo+json',
       },
+      body: JSON.stringify(requestBody),
     })
 
     if (!response.ok) {
