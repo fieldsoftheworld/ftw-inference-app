f6d3dfdb103b1da1d340275e9b9217b2e0880607
Andreas Hocevar <andreas.hocevar@gmail.com>
2025-08-17 13:33:22 +0200

    Add 'About' button and dialog

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,3 @@ import { RouterView } from 'vue-router'
 </template>
 
 <style scoped></style>
-
-<style>
-/* Global styles to make vueitfy elements look more like the existing elements.
- * In case all UI elements are converted to Vuetify, this should probably be removed.
- */
-.v-btn {
-  text-transform: none;
-  letter-spacing: normal;
-}
-</style>
