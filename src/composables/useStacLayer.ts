227e753069dfd6322fc98e51f97d92a92bac950d
Andreas Hocevar <andreas.hocevar@gmail.com>
2025-11-22 21:48:19 +0100

    Help distinguish scene overlay from base map

--- src/composables/useStacLayer.ts
+++ src/composables/useStacLayer.ts
@@ -17,11 +17,11 @@ export default function useStacLayer() {
           crossOrigin: 'anonymous',
           maxZoom: 18,
         }),
-        extent: extent,
+        extent,
+        // Set a semi-transparent background to help distinguish the tile from the base layer
+        background: 'rgba(0, 0, 0, 0.7)',
         zIndex: 100, // Place above base layer but below S2 grid
       })
-      // Set a semi-transparent background to help distinguish the tile from the base layer
-      currentStacLayer.value.setBackground('rgba(0, 0, 0, 0.4)')
       // Add the new layer to the map
       map.addLayer(currentStacLayer.value)
       // Fit the view to the transformed extent
