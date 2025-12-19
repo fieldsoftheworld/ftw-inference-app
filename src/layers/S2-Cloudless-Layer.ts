d9f2722b9f5a71ed4377e2d2528afd27cffee053
Matthias Mohr <webmaster@mamo-net.de>
2025-12-19 17:26:25 +0100

    Restrict years to the available years

--- src/layers/S2-Cloudless-Layer.ts
+++ src/layers/S2-Cloudless-Layer.ts
@@ -2,6 +2,11 @@ import TileLayer from 'ol/layer/Tile'
 import { XYZ } from 'ol/source'
 
 export default function createCloudlessLayer(year: number) {
+  if (year < 2016) {
+    year = 2016
+  } else if (year > 2024) {
+    year = 2024
+  }
   return new TileLayer({
     source: new XYZ({
       url: `https://tiles.maps.eox.at/wmts?layer=s2cloudless${year === 2016 ? '' : '-' + year}_3857&style=default&tilematrixset=GoogleMapsCompatible&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fjpeg&TileMatrix={z}&TileCol={x}&TileRow={y}`,
