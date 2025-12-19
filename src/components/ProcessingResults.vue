ce49cc1936538641b9607929cbf70181c8d0bd64
Matthias Mohr <webmaster@mamo-net.de>
2025-12-19 17:18:35 +0100

    fitToExtent unification, move buttons

--- src/components/ProcessingResults.vue
+++ src/components/ProcessingResults.vue
@@ -67,6 +67,16 @@
       </div>
 
       <div class="action-buttons">
+        <v-btn
+          icon
+          color="primary"
+          variant="flat"
+          @click.stop="returnToResultsHandler"
+          class="action-button return-to-results"
+          title="Zoom to results"
+        >
+          <v-icon :icon="mdiTarget"></v-icon>
+        </v-btn>
         <v-btn class="action-button download-results" @click="downloadResults" density="comfortable"
           >Download</v-btn
         >
@@ -77,15 +87,6 @@
           density="compact"
           >Clear</v-btn
         >
-        <v-btn
-          icon
-          color="primary"
-          variant="flat"
-          @click.stop="returnToResultsHandler"
-          class="action-button return-to-results"
-        >
-          <v-icon :icon="mdiTarget"></v-icon>
-        </v-btn>
       </div>
     </v-card-text>
   </v-card>
@@ -109,7 +110,7 @@ const emit = defineEmits<{
 }>()
 
 const { map, handleMapClick, vectorLayer, selectedFeature, hidePropertiesBox } = useMap()
-const { clearResults, returnToResults } = useAreaOfInterest()
+const { clearResults, returnToResults, fitToExtent } = useAreaOfInterest()
 const { showInfo, showError } = useNotifier()
 
 const isOpen = ref(true)
@@ -257,27 +258,7 @@ const fitMapToResult = (result: Feature) => {
   // Get the extent of the feature
   const extent = resultFeature.getGeometry()?.getExtent()
 
-  if (
-    !extent ||
-    extent.every((coord: number) => coord === 0) ||
-    extent.some((coord: number) => isNaN(coord))
-  ) {
-    showError('Invalid extent for this result.')
-    return
-  }
-
-  // Calculate dynamic padding based on screen dimensions
-  const screenWidth = window.innerWidth
-  const screenHeight = window.innerHeight
-  // Use a percentage of screen dimensions for padding
-  // This ensures the geometry fits regardless of screen size
-  const paddingY = Math.max(100, screenHeight * 0.2) // At least 100px or 30% of screen height
-  // Fit the map to the result's extent with dynamic padding
-  map.value?.getView().fit(extent, {
-    duration: 1000,
-    padding: [paddingY, screenWidth * 0.25, paddingY, screenWidth * 0.35], // [top, right, bottom, left]
-    maxZoom: 17,
-  })
+  fitToExtent(map.value!, extent, null)
 }
 
 onMounted(() => {
