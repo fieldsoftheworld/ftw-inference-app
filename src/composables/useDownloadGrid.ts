f0081f9611d4dc3669b932b5a4e09eec557e686a
Matthias Mohr <m.mohr@moregeo.it>
2026-04-22 22:24:21 +0200

    Add year as prefix to the download filename

--- src/composables/useDownloadGrid.ts
+++ src/composables/useDownloadGrid.ts
@@ -43,7 +43,7 @@ export function featureToGridCell(feature: FeatureLike): GridCell {
 function triggerDownload(url: string, filename: string) {
   const a = document.createElement('a')
   a.href = url
-  a.download = filename
+  a.download = settings.value.year + '-' + filename
   a.rel = 'noopener'
   document.body.appendChild(a)
   a.click()
